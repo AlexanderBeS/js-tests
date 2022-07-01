eb6c282eed304f6d7901c5341968c57beb8f3bad
AlexanderBeS <oibespalov@gmail.com>
2022-07-01 20:59:03 +0300

    init spread

---
+++ youtube-vladilen-minin/spread.js
@@ -0,0 +1,5 @@
+const citiesEurope = ['Berlin', 'Prague', 'Paris'];
+const citiesUkraine = ['Kyiv', 'Lviv', 'Dnipro'];
+
+console.log(citiesUkraine); //[ 'Kyiv', 'Lviv', 'Dnipro' ]
+console.log(...citiesUkraine); //Kyiv Lviv Dnipro
