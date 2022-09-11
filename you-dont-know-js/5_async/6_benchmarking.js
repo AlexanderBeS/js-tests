c230213a5878af228982fe24bfd535640e954dce
AlexanderBeS <oibespalov@gmail.com>
2022-09-11 15:06:39 +0300

    Finished with 5

---
+++ you-dont-know-js/5_async/6_benchmarking.js
@@ -0,0 +1,21 @@
+//Benchmark.js ( http://benchmarkjs.com/ )
+
+//function foo() {
+// 	// operation(s) to test
+// }
+//
+// var bench = new Benchmark(
+// 	"foo test",				// test name
+// 	foo,					// function to test (just contents)
+// 	{
+// 		// ..				// optional extra options (see docs)
+// 	}
+// );
+//
+// bench.hz;					// number of operations per second
+// bench.stats.moe;			// margin of error
+// bench.stats.variance;		// variance across samples
+
+
+//jsPerf ( http://jsperf.com ).
+
